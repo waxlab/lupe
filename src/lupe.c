557e789c63b571eb1439dc9d16f0cfdf57fb1532
Thadeu de Paula <thadeudepaula@gmail.com>
2022-09-24 15:58:45 -0300

    Fix: use of required Lua version binary for update

--- src/lupe.c
+++ src/lupe.c
@@ -12,28 +12,32 @@
 #define DEBUG printf
 
 
-static struct {
+
+static char lupe_script[PATH_MAX] = "\0"; /* Lua file with #! pointing to lupe*/
+static char lupe_rc[PATH_MAX]     = "\0"; /* the directory meta specifications */
+static char lupe_root[PATH_MAX]   = "\0"; /* the tree root directory */
+
+enum lupe_mode { lupe_mode_cli, lupe_mode_dir };
+
+/* Lua binary required */
+struct lupe_lua {
   char bin[PATH_MAX]; /* Lua binary path */
   char version[10];
+};
 
-  /* List containing the required Lua versions */
+/* List containing the required Lua versions */
+struct lupe_rc_lua {
   int verc;
   struct {
     char version[10]; /* Ex: 5.4 */
     char altvers[10]; /* Ex: 54 (BSD and other Unixes) */
   } verv[6];
-} Lua;
-
+};
 
 static struct {
   char item[100][PATH_MAX];
   int  len;
 } path_list;
-
-static char lupe_script[PATH_MAX] = "\0"; /* Lua file with #! pointing to lupe*/
-static char lupe_rc[PATH_MAX]   = "\0"; /* the directory meta specifications */
-static char lupe_root[PATH_MAX]   = "\0"; /* the tree root directory */
-static char ERROR[1024];
 
 /* matches the line version */
 static char const *lv_exp =
@@ -78,13 +82,13 @@ static int get_path_list(void) {
 
 
 /* Checks if version is valid example "5.1", ... "5.4" and populate @lvn */
-static void validate_lua_ver(void) {
+static void validate_lua_ver(struct lupe_rc_lua rc_lua) {
   char altvers[100] = "\0"; /* if ver="5.4" this will be "54" */
   int  ai       = 0;
   char *version;
 
-  for(int v=0; v < Lua.verc; v++) {
-    version=Lua.verv[v].version;
+  for(int v=0; v < rc_lua.verc; v++) {
+    version=rc_lua.verv[v].version;
     for(int c=0; c<strlen(version); c++) {
       if (version[c] >= '0' && version[c] <= '9') {
         altvers[ai++] = version[c];
@@ -93,41 +97,43 @@ static void validate_lua_ver(void) {
       if (version[c] != '.') lupe_error("Invalid version %s", version);
     }
     altvers[ai]='\0';
-    strcpy(Lua.verv[v].altvers, altvers);
+    strcpy(rc_lua.verv[v].altvers, altvers);
   }
 }
 
 
 
 /* Read file line by line until find the Lua required version */
-static void required_lua(void) {
+static struct lupe_rc_lua get_lupe_rc_lua(void) {
   FILE  *fp = fopen(lupe_rc, "r");
   char  *line  = NULL;
   size_t linelen = 0;
   int    linenum = 0;
   int    vv = 0;
-  Lua.verv[0].version[0] = '\0';
-  Lua.verc = 0;
+  static struct lupe_rc_lua rc_lua;
+  rc_lua.verv[0].version[0] = '\0';
+  rc_lua.verc = 0;
 
   if (fp) {
     while (getline(&line, &linelen, fp) != -1 && linenum++ < 3) {
       vv = sscanf(line, lv_exp,
-                  Lua.verv[0].version, Lua.verv[1].version,
-                  Lua.verv[2].version, Lua.verv[3].version,
-                  Lua.verv[4].version, Lua.verv[5].version);
+                  rc_lua.verv[0].version, rc_lua.verv[1].version,
+                  rc_lua.verv[2].version, rc_lua.verv[3].version,
+                  rc_lua.verv[4].version, rc_lua.verv[5].version);
       if (vv) {
-        Lua.verc = vv;
-        validate_lua_ver();
+        rc_lua.verc = vv;
+        validate_lua_ver(rc_lua);
         break;
       }
     }
     free(line);
     fclose(fp);
 
-    if (Lua.verv[0].version[0] == '\0') {
+    if (rc_lua.verv[0].version[0] == '\0') {
       lupe_error("no valid 'lua' entry found at the beginning of '%s'", lupe_rc);
     }
   }
+  return rc_lua;
 }
 
 
@@ -173,29 +179,35 @@ static int find_anylua(char *filename, char* ver) {
   }
 }
 
-static int whereis_lua(void) {
-  required_lua();
+
+
+static struct lupe_lua whereis_lua(void) {
+  struct lupe_rc_lua rc_lua = get_lupe_rc_lua();
+  struct lupe_lua    lua;
+  strcpy(lua.bin,    "\0");
+  strcpy(lua.version,"\0");
+
   int v=0; /* keep at this scope! */
-  for ( ; v < Lua.verc; v++) {
+  for ( ; v < rc_lua.verc; v++) {
     for (int p=0; p < path_list.len; p++) {
       /* Check if is lua5.1 .. lua5.4 */
-      sprintf(Lua.bin, "%s/%s%s", path_list.item[p], "lua", Lua.verv[v].version);
-      if (0 == access(Lua.bin, X_OK)) goto found;
+      sprintf(lua.bin, "%s/%s%s", path_list.item[p], "lua", rc_lua.verv[v].version);
+      if (0 == access(lua.bin, X_OK)) goto found;
 
       /* Check if is lua51 .. lua54 */
-      sprintf(Lua.bin,"%s/%s%s", path_list.item[p], "lua", Lua.verv[v].altvers);
-      if (0 == access(Lua.bin, X_OK)) goto found;
+      sprintf(lua.bin,"%s/%s%s", path_list.item[p], "lua", rc_lua.verv[v].altvers);
+      if (0 == access(lua.bin, X_OK)) goto found;
 
       /* Check if is lua and matches Lua _VERSION */
-      sprintf(Lua.bin, "%s/%s", path_list.item[p], "lua");
-      if(0 == find_anylua(Lua.bin, Lua.verv[v].version)) goto found;
+      sprintf(lua.bin, "%s/%s", path_list.item[p], "lua");
+      if(0 == find_anylua(lua.bin, rc_lua.verv[v].version)) goto found;
     }
   }
   lupe_error("No Lua version for the '%s' tree was found in your PATH", lupe_root);
-  return 1;
+
   found:
-    strcpy(Lua.version, Lua.verv[v].version);
-    return 0;
+    strcpy(lua.version, rc_lua.verv[v].version);
+    return lua;
 }
 
 /* If string terminates with \n remove it and returns */
@@ -211,7 +223,7 @@ static char *trimnl(char *s) {
 ** is processed. For the lupe module has access to the argument list, it is
 ** emulated trough the LUPE_ARG[*] set of variables.
 */
-static void lupe_env(int argc, char **argv) {
+static void lupe_env(struct lupe_lua lua, int argc, char **argv) {
   char *lua_argf = "LUA_ARG[%d]";
   char *lua_argi = malloc(sizeof(char) * sysconf(_SC_ARG_MAX));
 
@@ -226,7 +238,7 @@ static void lupe_env(int argc, char **argv) {
   }
   /* interpreter is the -1 */
   sprintf(lua_argi, lua_argf, -1);
-  setenv(lua_argi, Lua.bin, 1);
+  setenv(lua_argi, lua.bin, 1);
 
   /* unset the argc+1... the principle of the non-sparse list ended in nil */
   sprintf(lua_argi, lua_argf, argc);
@@ -236,13 +248,13 @@ static void lupe_env(int argc, char **argv) {
 
 
 
-static void call_lua(int argc, char **argv) {
+static void call_lupe_lua(struct lupe_lua lua, enum lupe_mode mode, int argc, char **argv) {
   char *argl[1024];
   /* arguments passed to Lua */
-  argl[0] = Lua.bin;
+  argl[0] = lua.bin;
   argl[1] = "-l";
   argl[3] = "--";
-  if (lupe_script[0] == '\0') {
+  if (mode == lupe_mode_cli) {
     argl[2] = "lupe.cli";
     argl[4] = "/dev/null";
   } else {
@@ -251,67 +263,52 @@ static void call_lua(int argc, char **argv) {
   }
 
   for( int i=1; i<argc; i++) argl[i+4]=argv[i];
-  lupe_env(argc, argv);
+  lupe_env(lua, argc, argv);
   execvp(argl[0], argl);
   lupe_error("'exec %s': %s", argl[0], strerror(errno));
 }
 
+static void set_lupe_script(char *path) {
+  struct stat st;
+  if (realpath(path,lupe_script) == NULL) {
+    lupe_error("Error accessing '%s' : %s", path, strerror(errno));
+  }
+}
 
 /* resolve the real location for the script */
 static void resolve(char *path) {
   struct stat st;
-  char ps[PATH_MAX];
-  char *dir;
-
-  if (realpath(path, lupe_script) == NULL)
-    lupe_error("Error accessing '%s' : %s", path, strerror(errno));
-
-  stat(lupe_script, &st);
-
+  stat (path, &st);
   if (S_ISDIR(st.st_mode)) {
-    strcat(lupe_script,"/bin/main.lua");
-    if ( stat(lupe_script, &st) != 0 ) lupe_error("Error accessing script '%s' : %s", lupe_script, strerror(errno));
-  }
-
-  if (! (S_ISREG(st.st_mode))) lupe_error("Error: '%s' must be a regular file", lupe_script);
-
-  strcpy(ps, lupe_script);
-  dir = dirname(ps);
-
-  for (int i = strlen(dir); i>0; i--) {
-    if (dir[i] == '/') {
-      if (strcmp(&dir[i], "/bin") == 0) {
-        dir[i]='\0';
-        strcpy(lupe_root, dir);
-        break;
+    strcpy(lupe_root, path);
+    sprintf(lupe_script, "%s/bin/main.lua", lupe_root);
+  } else if (S_ISREG(st.st_mode)) {
+    strcpy(lupe_script, path);
+    char *dir = dirname(path);
+    for (int i = strlen(dir); i>0; i--) {
+      if (dir[i] == '/') {
+        if (strcmp(&dir[i], "/bin") == 0) {
+          dir[i]='\0';
+          strcpy(lupe_root, dir);
+          break;
+        }
       }
     }
+    if ( lupe_root[0] == '\0' ) {
+      lupe_error("The script '%s' doesn't belongs to a Lupe tree", lupe_script);
+    }
+  } else {
+    lupe_error("Error: '%s' must be a regular file", lupe_script);
   }
-  if ( lupe_root[0] == '\0' ) {
-    lupe_error("The script '%s' doesn't belongs to a Lupe tree", lupe_script);
-  }
-
-  sprintf(lupe_rc, "%s/%s", lupe_root, "luperc.lua");
-  if ( stat(lupe_rc, &st) != 0 ) {
+  sprintf(lupe_rc, "%s/luperc.lua", lupe_root);
+  if (stat(lupe_rc, &st) != 0) {
     lupe_error("Error detecting '%s' : %s", lupe_rc, strerror(errno));
   }
-  if( S_ISREG(st.st_mode) ) return;
-  lupe_error("The lupe file wasn't found at '%s'", lupe_root);
-}
-
-
-
-/* Process the shebanged file or directory */
-static int lupe_shell(int argc, char **argv) {
-  resolve(argv[0]);
-
-  if (whereis_lua() == 0) {
-    call_lua(argc, argv);
+  if (! S_ISREG(st.st_mode) ) {
+    lupe_error("The lupe file wasn't found at '%s'", lupe_root);
   }
-
-  fprintf(stderr, "Not found a Lua version");
-  return 1;
 }
+
 
 
 /* For future commands... check if string has dir separators */
@@ -324,30 +321,33 @@ static int ispath(char *str) {
 
 
 int main(int argc, char **argv) {
-  /*
-  ** Lua requires a filename to be processed after or it falls
-  ** into the repl. So lupe_script should be a Lua file or /dev/null
-  */
 
-  strcpy(Lua.bin,    "\0");
-  strcpy(Lua.version,"\0");
-  Lua.verc = 0;
+  char target_path[PATH_MAX];
+  struct lupe_lua lua;
+  enum lupe_mode mode;
+  get_path_list();
 
-  if ( argc > 1 && ispath(argv[1]) ) {
-    get_path_list();
-    lupe_shell(argc-1, &argv[1]);
-    return 0;
+  if (argc > 1 && ispath(argv[1])) {
+    mode = lupe_mode_dir;
+    strcpy(target_path, argv[1]);
+    if (realpath(target_path, target_path) == NULL) {
+      lupe_error("Error accessing '%s' : %s", target_path, strerror(errno));
+    }
+    argc--; argv = &argv[1];
+    resolve(argv[0]);
+    lua = whereis_lua();
+  } else {
+    getcwd(target_path,PATH_MAX);
+    mode = lupe_mode_cli;
+    if (strcmp(argv[1], "help") == 0 || strcmp(argv[1], "init") == 0) {
+      strcpy(lua.bin,     "lua");
+      strcpy(lua.version, "\0");
+    } else {
+      resolve(target_path);
+      lua = whereis_lua();
+    }
   }
-
-  char *argvdef[2] = { argv[0], "help" };
-  if (argc < 2) {
-    argc = 2;
-    argv = argvdef;
-  }
-  getcwd(lupe_root, PATH_MAX);
-  sprintf(lupe_rc, "%s/%s", lupe_root, "luperc.lua");
-  strcpy(Lua.bin, "lua");
-  call_lua(argc, argv);
+  call_lupe_lua( lua, mode, argc, argv );
   return 0;
 }
 
